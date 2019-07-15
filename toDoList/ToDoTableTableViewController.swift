1b9db33d364071927526ba8e38495703d472a8fc
Apple <apple@Apples-MBP-108.attlocal.net>
2019-07-15 11:17:01 -0700

    Complete Iteration 1

---
+++ toDoList/ToDoTableTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ToDoTableTableViewController.swift
+//  toDoList
+//
+//  Created by Apple on 7/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableTableViewController: UITableViewController {
+    
+    
+    var toDos: [ToDo] = []
+ var previousVc = ToDoTableTableViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+      
+    }
+
+    func createToDo () -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the dog"
+        
+        return [swift, dog]
+    }
+    
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let addVC = segue.destination as? AddToDoViewController {
+            addVC.previousVC = self
+        }
+        
+        if let completeVc = segue.destination as? CompleteToDoViewController {
+        if let toDo = sender as? ToDo {
+            completeVC = selectedToDo = toDo
+            completeVC.previousVC = self
+        }
+    }
+    
+    
+
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let toDo = toDos[indexPath.row]
+        performSegue(withIdentifier: "move to complete", sender: toDo)
+    }
+     
+     
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ toDoList/toDo.swift
@@ -0,0 +1,15 @@
+//
+//  toDo.swift
+//  toDoList
+//
+//  Created by Apple on 7/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+}
